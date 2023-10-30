d3360315451e9f9ab25b5eb7525ae93f5f3a0360
Saurav Sharma <saurav5122000@gmail.com>
2023-10-30 20:58:58 +0530

    developement project

--- Propertico/resources/views/layout/app.blade.php
+++ Propertico/resources/views/layout/app.blade.php
@@ -33,7 +33,7 @@
 <div class="container">
 <div class="row align-items-center">
 <div class="col-6 col-xl-2">
-<h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Propertico</a></h1>
+<h1 class="mb-0 site-logo m-0 p-0"><a href="/home" class="mb-0">Propertico</a></h1>
 </div>
 <div class="col-12 col-md-10 d-none d-xl-block">
 <nav class="site-navigation position-relative text-right" role="navigation">
