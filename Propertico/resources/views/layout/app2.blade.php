d4f816f19a82bec0758e691d3571ada9c6557ed8
Saurav Sharma <saurav5122000@gmail.com>
2023-11-19 09:28:11 +0530

    developement project with MVC

--- Propertico/resources/views/layout/app2.blade.php
+++ Propertico/resources/views/layout/app2.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-<title>Warehouse &mdash; Website Template by Colorlib</title>
+<title>Propertico</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
